2952fb7d22246f2c467fb1ab5c277736b385ec24
theacousticguy <42766600+abhishekS14@users.noreply.github.com>
2024-06-16 20:46:06 +0530

    Added word count functionality

--- React/word-case-converter/src/components/Converter.js
+++ React/word-case-converter/src/components/Converter.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 export default function Converter() {
+    const [text, setText] = useState("Enter text here");
+    const [count, setCount] = useState(0);
+
     const handleUpClick = () => {
         setText(text.toUpperCase());
     }
@@ -9,19 +12,33 @@ export default function Converter() {
         setText(text.toLowerCase());
     }
 
+    const handleCountClick = () => {
+        if (text.length === 0) {
+            setCount(0);
+        } else {
+
+            text.endsWith(" ") ? setCount(text.split(" ").length - 1) : setCount(text.split(" ").length);
+        }
+    }
+
     const handleChange = (event) => {
         setText(event.target.value);
     }
 
-    const [text, setText] = useState("Enter text here");
 
     return (
         <>
+            <div>
+                <div className="mb-3">
+                    <textarea className="form-control" rows="6" value={text} onChange={handleChange}></textarea>
+                </div >
+                <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleUpClick} >Convert to Uppercase</button>
+                <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleLoClick} >Convert to Lowercase</button>
+                <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleCountClick} >Word Count</button>
+            </div>
             <div className="mb-3">
-                <textarea className="form-control" rows="6" value={text} onChange={handleChange}></textarea>
+                <textarea className="form-control" rows="1" value={count} onChange={handleChange}></textarea>
             </div >
-            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleUpClick} >Convert to Uppercase</button>
-            <button type="button" className="btn btn-primary my-3 mx-3" onClick={handleLoClick} >Convert to Lowercase</button>
         </>
 
 
